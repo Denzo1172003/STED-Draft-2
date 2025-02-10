0d9cfb434a612fa38f8bb98eb181b14dda01fd17
Denzo1172003 <108604833+Denzo1172003@users.noreply.github.com>
2025-02-10 09:03:40 +0800

    Update app.js

--- app.js
+++ app.js
@@ -55,3 +55,26 @@ thumbnails.forEach((thumbnail, index) => {
         showSlider();
     })
 })
+
+
+
+
+
+// SHOW MENU 
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+/* Menu Show */
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+};
+
+/* Menu Hidden */
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+};
